e34cff92b7f1ba61696df5e1d46e82765f0afbbb
mdemon420 <mdeamon4@gmail.com>
2022-04-02 23:35:39 +0600

    add sibiling component

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (props) => {
+    const {count,setCount} = props;
+    return (
+        <div>
+            <h1>This is a header: {count}</h1>
+            <button onClick={()=>setCount(count+1)}>increase</button>
+        </div>
+    );
+};
+
+export default Header;
